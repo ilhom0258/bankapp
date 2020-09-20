6c9e46822b6db5f791d6d8fc22b2696cc2555bc9
ilhom0258 <ilhom0258@gmail.com>
2020-09-20 13:45:15 +0500

    Github Actions implementations

--- cmd/bank/main.go
+++ cmd/bank/main.go
@@ -1,17 +1,5 @@
 package main
 
-<<<<<<< HEAD
 func main() {
-
-=======
-import (
-	"bank/pkg/bank/card"
-	"bank/pkg/bank/types"
-	"fmt"
-)
-
-func main() {
-	card := card.IssueCard(types.TJS, "black", "Infinity")
-	fmt.Println(card)
->>>>>>> 8984773dea0a3995a7ba94c524ebaa8a3ade4843
+	
 }
